dcabc83fe017c30ec48e39e357809748fb2d8b95
Hepheir <hepheir@gmail.com>
2018-12-05 22:13:03 +0900

    .

---
+++ 2018-2학기/프로그래밍실습2/Lecture/181203/15-6.c
@@ -0,0 +1,45 @@
+// wb는 binary모드로 write
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct personscore
+{
+    int number;
+    char name[40];
+    int mid;
+    int final;
+    int quiz;
+};
+typedef struct personscore pscore;
+
+int main()
+{
+    char fname[] = "score.bin";
+    FILE *f;
+
+    if (fopen_s(&f, fname, "wb") != 0)
+    {
+        printf("파일이 열리지 않습니다.\n");
+        exit(1);
+    };
+    
+    char line[80];
+    int cnt = 0;
+    pscore score;
+    printf("이름과 성적(중간, 기말, 퀴즈)을 입력하세요.\n");
+
+    fgets(line, 80, stdin);
+    while (!feof(stdin))
+    {
+        sscanf_s(line, "%s %d %d %d", score.name, 40,
+            &score.mid, &score.final, &score.quiz);
+        score.number = ++cnt;
+        fwrite(&score, sizeof(pscore), 1, f);
+        fgets(line, 80, stdin);
+    }
+    fclose(f);
+
+    return 0;
+}
---
+++ 2018-2학기/프로그래밍실습2/Lecture/181203/15-7.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+struct personscore
+{
+    int number;
+    char name[40];
+    int mid;
+    int final;
+    int quiz;
+};
+typedef struct personscore pscore;
+void printhead();
+
+int main()
+{
+    char fname[] = "score.bin";
+    FILE *f;
+
+    if ((f = fopen(fname, "rb")) == NULL)
+    {
+        printf("파일이 열리지 않습니다.\n");
+        exit(1);
+    }
+    printhead();
+
+    pscore score;
+    fread(&score, sizeof(pscore), 1, f);
+    while (!feof(f))
+    {
+        fprintf(stdout, "%6d%18s%8d%8d%8d\n",
+            score.number, score.name, score.mid, score.final, score.quiz);
+        fread(&score, sizeof(pscore), 1, f);
+    }
+    printf("%s\n", " -----------------------------------------------");
+    fclose(f);
+
+    return 0;
+}
+
+void printhead()
+{
+    printf("%s\n", " -----------------------------------------------");
+    printf("%8s%15s%10s%8s%8s", "번호", "이름", "중간", "기말", "퀴즈");
+    printf("%s\n", " -----------------------------------------------");
+}
