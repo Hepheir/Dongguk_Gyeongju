6610cd48efa2dea0ca01732bf1294c29cfa4fd19
Hepheir <hepheir@gmail.com>
2018-12-12 20:34:29 +0900

    Add Color printing / Highlighting & Better logics

--- 2018-2학기/전공워밍업-파이썬/Project/Minesweeper.py
+++ 2018-2학기/전공워밍업-파이썬/Project/Minesweeper.py
@@ -1,6 +1,10 @@
 # 지뢰찾기 프로그램 by Hepheir
 
 import random
+from colorama import init, Fore, Back, Style
+
+# colorma initialize
+init()
 
 # Constant
 BLANK = '.'
@@ -25,7 +29,7 @@ Count = 0
 def main():
     while True:
         run()
-        if (input("게임을 계속 하시겠습니까? (y/n)") == 'n'):
+        if (input("게임을 계속 하시겠습니까? (y/n): ") == 'n'):
             break
 
 def run():
@@ -35,27 +39,54 @@ def run():
     setMineField()
     Count = 0
 
+    highlightBox = None
+
+    act = None
+    x = 0
+    y = 0
     while True:
-        printMap()
+        printMap(highlightBox)
 
-        x = int(input("x 좌표 (1~%d): " % MapWidth)) - 1
-        y = int(input("y 좌표 (1~%d): " % MapHeight)) - 1
-        act = int(input("1. 열기\n2. 깃발 세우기/제거하기\n3. 종료\n>>> "))
+        print("------------ Action ------------")
+        print("1. 열기")
+        print("2. 깃발 세우기/제거하기")
+        print("3. 강조구역 설정")
+        print("0. 종료")
+        act = int(input(">>> "))
 
-        if act == 1:
-            openTile(x, y)
-        elif act == 2:
-            flagTile(x, y)
-        elif act == 3:
-            # 종료
+        # 종료
+        if act == 0:
             break
 
+        # 강조 할 구역 재설정
+        if act == 3:
+            h_x1, h_y1, h_x2, h_y2 = [int(k) for k in input("(x1, y1, x2, y2): ").split()]
+            highlightBox = (h_x1, h_y1, h_x2, h_y2)
+        
+        elif act == 1 or act == 2:
+            # 작업을 처리할 좌표 입력
+            x = int(input("x 좌표 (1~%d): " % MapWidth)) - 1
+            y = int(input("y 좌표 (1~%d): " % MapHeight)) - 1
+
+            if act == 1:
+                openTile(x, y)
+
+            elif act == 2:
+                flagTile(x, y)
+
+        # 잘못된 입력에 대한 예외 처리
+        else:
+            print("Invalid Input")
+            continue
+
+        Tile = Map[x][y]
+
         # 지뢰가 터졌을 경우, 모든 타일을 열고 필드를 출력해 준 후에 게임 종료.
-        if Map[x][y] == MINE_GAMEOVER:
+        if Tile == MINE_GAMEOVER:
             # 처음부터 지뢰가 있는 타일을 골랐을 경우, 지뢰를 터트리지 않고 새로운
             # 맵을 생성함.  
             if Count == 0:
-                while Map[x][y] != MINE and Map[x][y] != MINE_GAMEOVER:
+                while Tile != MINE and Tile != MINE_GAMEOVER:
                     setMineField()
                 openTile(x, y)
             # 게임 오버
@@ -103,7 +134,6 @@ def openTile(x, y):
 
     # 열린 타일에 주변에 있는 지뢰의 수를 표시
     mines = searchMinesAround(x, y)
-    print('mines: %d' % mines)
     Map[x][y] = mines
 
     # 주변에 지뢰가 없으면, 주변의 타일이 함께 열리는 보너스
@@ -157,26 +187,6 @@ def flagTile(x, y):
         Map[x][y] = BLANK
     else:
         Map[x][y] = FLAGGED
-    
-
-class C_COLORS:
-    # HEADER = '\033[95m'
-    # OKBLUE = '\033[94m'
-    # OKGREEN = '\033[92m'
-    # WARNING = '\033[93m'
-    # FAIL = '\033[91m'
-    # ENDC = '\033[0m'
-    # BOLD = '\033[1m'
-    # UNDERLINE = '\033[4m'
-    RED   = "\033[1;31m" #3  
-    BLUE  = "\033[1;34m" #1
-    CYAN  = "\033[1;36m" #4,5,6,7,8...
-    GREEN = "\033[0;32m" #2
-    RESET = "\033[0;0m" 
-    BOLD    = "\033[;1m"
-    REVERSE = "\033[;7m"
-    WARNING = '\033[93m'
-    FAIL = '\033[91m'
 
 def printMap(highlightBox=None):
     # 사용자가 보기 쉽게 필드를 출력하는 함수
@@ -186,50 +196,105 @@ def printMap(highlightBox=None):
 
     if not highlightBox == None:
         (x1, y1, x2, y2) = highlightBox
-    else:
-        isHighlight = False
 
-    charColor = C_COLORS.RESET
-
-    for y in range(MapHeight):
-        for x in range(MapWidth):
-            if not isHighlight == False:
-                isHighlight = (y1 <= y and y < y2) and (x1 <= x and x < x2)
-
-            if Map[x][y] == BLANK or Map[x][y] == MINE:
-                tile = SKIN_COVERED[0] + '_' + SKIN_COVERED[1]
-                charColor = C_COLORS.RESET
-
-            elif Map[x][y] == FLAGGED or Map[x][y] == MINE_FLAGGED:
-                tile = SKIN_COVERED[0] + '!' + SKIN_COVERED[1]
-                charColor = C_COLORS.WARNING
-
-            elif Map[x][y] == 0:
-                tile = SKIN_OPENED[0] + ' ' + SKIN_OPENED[1]
-                charColor = C_COLORS.RESET
-
-            elif Map[x][y] == MINE_GAMEOVER:
-                tile = SKIN_GAMEOVER[0] + MINE + SKIN_GAMEOVER[1]
-                charColor = C_COLORS.FAIL
-
-            else:
-                TileNum = Map[x][y]
-                if TileNum == 1:
-                    charColor = C_COLORS.BLUE
-                elif TileNum == 2:
-                    charColor = C_COLORS.GREEN
-                elif TileNum == 3:
-                    charColor = C_COLORS.RED
-                else:
-                    charColor = C_COLORS.CYAN
-
-                tile = SKIN_OPENED[0] + str(Map[x][y]) + SKIN_OPENED[1]
+    tile_content = ''
+    tile_color = None
+    tile_backgroundColor = None
+    tile_style = None
     
-            if isHighlight:
-                print(C_COLORS.BOLD, end='')
+    print("------------ Mine Field ------------")
+    for y in range(MapHeight):
+        # 보드의 x, y의 칸 번호 안내 표시
+        if y == 0:
+            print("   ", end='')
+            for x in range(MapWidth):
+                print(" %-2d" % (x + 1), end='')
+            print("")
+        print("%2d " % (y + 1), end='')
 
-            print(charColor, end='')
-            print(tile + C_COLORS.RESET, end='')
+        for x in range(MapWidth):
+            # 만약 하이라이트 구간이 정해져 있으면, 지금 x, y값이 하이라이트 구간 내부인지 확인
+            if not highlightBox == None:
+                isHighlight = (y1 <= y and y < y2) and (x1 <= x and x < x2)
+            else:
+                isHighlight = False
+
+            # 열리지 않은 칸 표시
+            if Map[x][y] == BLANK or Map[x][y] == MINE:
+                tile_content = SKIN_COVERED[0] + '_' + SKIN_COVERED[1]
+                
+                tile_color = None
+                tile_backgroundColor = None
+
+            # 깃발이 꽃혀있는 칸 표시
+            elif Map[x][y] == FLAGGED or Map[x][y] == MINE_FLAGGED:
+                tile_content = SKIN_COVERED[0] + FLAGGED + SKIN_COVERED[1]
+                
+                tile_color = Fore.YELLOW
+                tile_backgroundColor = None
+
+            # 터트린 지뢰 표시
+            elif Map[x][y] == MINE_GAMEOVER:
+                tile_content = SKIN_GAMEOVER[0] + MINE + SKIN_GAMEOVER[1]
+                
+                tile_color = Fore.BLACK
+                tile_backgroundColor = Back.RED
+
+            # 열린 칸 - 주변에 지뢰가 없는 빈 칸 표시
+            elif Map[x][y] == 0:
+                tile_content = SKIN_OPENED[0] + ' ' + SKIN_OPENED[1]
+                
+                tile_color = Fore.WHITE
+                tile_backgroundColor = None
+
+            # 일반 열린 타일은 각 숫자에 해당되는 색상으로 출력
+            else:
+                tile_num = Map[x][y]
+                tile_content = SKIN_OPENED[0] + str(tile_num) + SKIN_OPENED[1]
+
+                if tile_num == 1:
+                    tile_color = Fore.BLUE
+
+                elif tile_num == 2:
+                    tile_color = Fore.GREEN
+                    
+                elif tile_num == 3:
+                    tile_color = Fore.RED
+                    
+                elif tile_num == 4:
+                    tile_color = Fore.BLUE
+                    
+                else:
+                    tile_color = Fore.CYAN
+
+                tile_backgroundColor = Back.BLACK
+
+            # 하이라이트 구간은 볼드체로 표시
+            if isHighlight:
+                tile_style = Style.BRIGHT
+                tile_color = Fore.MAGENTA
+            else:
+                tile_style = Style.DIM
+
+            # 기본 색상, 기본 배경
+            if tile_color == None:
+                tile_color = Fore.RESET
+
+            if tile_backgroundColor == None:
+                tile_backgroundColor = Back.RESET
+
+            # 타일을 출력하고, 색상을 다시 원래대로 돌림
+            print(
+                tile_style +
+                tile_color +
+                tile_backgroundColor +
+
+                tile_content +
+
+                Fore.RESET +
+                Back.RESET +
+                Style.RESET_ALL,
+            end='')
         print('')
 
 def openAll():
