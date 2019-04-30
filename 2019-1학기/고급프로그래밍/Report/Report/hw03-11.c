7d873c88ea9b938b48425933d0caf7395a06420c
김동주 <hepheir@gmail.com>
2019-04-30 11:45:28 +0900

    .

--- 2019-1학기/고급프로그래밍/Report/Report/hw03-11.c
+++ 2019-1학기/고급프로그래밍/Report/Report/hw03-11.c
@@ -1,4 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <math.h>
+
+int factorial(int x);
+double bern(double p, int n, int x);
 
 void hw03_11() {
-	//
+	double p;
+	int n;
+
+	printf("성공할 확률 (0 < p < 1) : ");
+	scanf("%lf", &p);
+	printf("시행 횟수 (0 < n) : ");
+	scanf("%d", &n);
+
+	printf("적어도 1번 사고날 확률 : %.5lf\n",
+		1 - bern(p, n, 0)
+	);
+	printf("많아야 3번 사고날 확률 : %.5lf\n",
+		bern(p, n, 0)
+		+ bern(p, n, 1)
+		+ bern(p, n, 2)
+		+ bern(p, n, 3)
+	);
+}
+
+int factorial(int x) {
+	if (x == 0)
+		return 1;
+	return x * factorial(x - 1);
+}
+
+double bern(double p, int n, int x) {
+	double b = pow(p, x) * pow(1 - p, n - x);
+	double nx = factorial(n) / (double)(factorial(x) * factorial(n - x));
+	return b * nx;
 }
--- 2019-1학기/고급프로그래밍/Report/Report/hw03-12.c
+++ 2019-1학기/고급프로그래밍/Report/Report/hw03-12.c
@@ -1,4 +1,59 @@
+#include <stdio.h>
+void matSum3by3();
+void matSub3by3();
+void matMult3by3();
+
+int A[3][3] = {
+	{ 1, 2, 3 },
+	{ 4, 5, 6 },
+	{ 7, 8, 9 }
+};
+
+int B[3][3] = {
+	{ 1, 0, 0 },
+	{ 2, 1, 0 },
+	{ 3, 2, 1 }
+};
 
 void hw03_12() {
-	//
+
+	printf("A + B :\n");
+	matSum3by3(A, B);
+
+	printf("A - B :\n");
+	matSub3by3(A, B);
+
+	printf("A * B :\n");
+	matMult3by3(A, B);
+
+}
+
+void matSum3by3() {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++)
+			printf(" [%3d]", A[i][j] + B[i][j]);
+		printf("\n");
+	}
+}
+
+void matSub3by3() {
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++)
+			printf(" [%3d]", A[i][j] - B[i][j]);
+		printf("\n");
+	}
+}
+
+void matMult3by3() {
+	int cell;
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 3; j++) {
+			cell = 0;
+			for (int k = 0; k < 3; k++) {
+				cell += A[i][k] * B[k][j];
+			}
+			printf(" [%3d]", cell);
+		}
+		printf("\n");
+	}
 }
